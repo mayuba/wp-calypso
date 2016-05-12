e13cc971eb7d44be7c9f6592fff83ed1c322d3f2
Ben Lowery <ben.lowery@automattic.com>
2016-05-11 22:16:58 -0400

    night hacking

--- client/state/reader/feeds/reducer.js
+++ client/state/reader/feeds/reducer.js
@@ -1,13 +1,21 @@
 import { combineReducers } from 'redux';
 import assign from 'lodash/assign';
+import omitBy from 'lodash/omitBy';
 
 import {
 	READER_FEED_REQUEST,
 	READER_FEED_REQUEST_SUCCESS,
-	READER_FEED_REQUEST_FAILURE
+	READER_FEED_REQUEST_FAILURE,
+	DESERIALIZE,
+	SERIALIZE
 } from 'state/action-types';
 
+import { isValidStateWithSchema } from 'state/utils';
+import { itemsSchema } from './schema';
+
 const actionMap = {
+	[ SERIALIZE ]: handleSerialize,
+	[ DESERIALIZE ]: handleDeserialize,
 	[ READER_FEED_REQUEST_SUCCESS ]: handleRequestSuccess,
 	[ READER_FEED_REQUEST_FAILURE ]: handleRequestFailure
 };
@@ -16,21 +24,39 @@ function defaultHandler( state ) {
 	return state;
 }
 
-function handleRequestFailure( state, action ) {
-	return state;
+function handleSerialize( state ) {
+	// remove errors from the serialized state
+	return omitBy( state, 'is_error' );
 }
 
-function handleRequestSuccess( state, action ) {
+function handleDeserialize( state ) {
+	if ( isValidStateWithSchema( state, itemsSchema ) ) {
+		return state;
+	}
+	return {};
+}
+
+function handleRequestFailure( state, action ) {
+	// new object proceeds current state to prevent new errors from overwriting existing values
 	return assign( {
-		[ action.payload.feed_ID ]: action.payload
+		[ action.meta.feed_ID ]: {
+			feed_ID: action.meta.feed_ID,
+			is_error: true
+		}
 	}, state );
 }
 
-export function entries( state = {}, action ) {
+function handleRequestSuccess( state, action ) {
+	return assign( {}, state, {
+		[ action.payload.feed_ID ]: action.payload
+	} );
+}
+
+export function items( state = {}, action ) {
 	const handler = actionMap[ action.type ] || defaultHandler;
 	return handler( state, action );
 }
 
 export default combineReducers( {
-	entries
+	items
 } );
--- client/state/reader/feeds/test/reducer.js
+++ client/state/reader/feeds/test/reducer.js
@@ -2,6 +2,8 @@
  * External dependencies
  */
 import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
+import sinon from 'sinon';
 
 /**
  * Internal dependencies
@@ -9,49 +11,129 @@ import { expect } from 'chai';
 import {
 	READER_FEED_REQUEST,
 	READER_FEED_REQUEST_SUCCESS,
-	READER_FEED_REQUEST_FAILURE
+	READER_FEED_REQUEST_FAILURE,
+	SERIALIZE,
+	DESERIALIZE
 } from 'state/action-types';
 
 import {
-	entries
+	items
 } from '../reducer';
 
 describe( 'reducer', () => {
-	it( 'should return an empty map by default', () => {
-		expect( entries( undefined, {} ) ).to.equal( {} );
-	} );
+	describe( 'items', () => {
+		it( 'should return an empty map by default', () => {
+			expect( items( undefined, {} ) ).to.deep.equal( {} );
+		} );
 
-	it( 'should update the state when receiving a feed', () => {
-		expect(
-			entries( {}, {
+		it( 'should update the state when receiving a feed', () => {
+			expect(
+				items( {}, {
+					type: READER_FEED_REQUEST_SUCCESS,
+					payload: {
+						feed_ID: 1,
+						blog_ID: 2
+					}
+				} )[ 1 ]
+			).to.deep.equal( {
+				feed_ID: 1,
+				blog_ID: 2
+			} );
+		} );
+
+		it( 'should serialize feed entries', () => {
+			const unvalidatedObject = deepFreeze( { hi: 'there' } );
+			expect( items( unvalidatedObject, { type: SERIALIZE } ) ).to.deep.equal( unvalidatedObject );
+		} );
+
+		it( 'should not serialize errors', () => {
+			const stateWithErrors = deepFreeze( {
+				12: { feed_ID: 12 },
+				666: { feed_ID: 666, is_error: true }
+			} );
+			expect( items( stateWithErrors, { type: SERIALIZE } ) ).to.deep.equal( {
+				12: { feed_ID: 12 }
+			} );
+		} );
+
+		it( 'should reject deserializing entries it cannot validate', sinon.test( function() {
+			const unvalidatedObject = deepFreeze( { hi: 'there' } );
+			this.stub( console, 'warn' ); // stub warn to suppress the warning that validation failure emits
+			expect( items( unvalidatedObject, { type: DESERIALIZE } ) ).to.deep.equal( {} );
+		} ) );
+
+		it( 'should deserialize good things', () => {
+			const validState = deepFreeze( {
+				1234: {
+					feed_ID: 1234,
+					blog_ID: 4567,
+					name: 'Example Dot Com',
+					URL: 'http://example.com',
+					feed_URL: 'http://example.com/feed',
+					subscribers_count: 10,
+					is_following: false
+				}
+			} );
+			expect( items( validState, { type: DESERIALIZE } ) ).to.deep.equal( validState );
+		} );
+
+		it( 'should stash an error object in the map if the request fails', () => {
+			expect( items( {}, {
+				type: READER_FEED_REQUEST_FAILURE,
+				error: true,
+				payload: new Error( 'request failed' ),
+				meta: {
+					feed_ID: 666
+				}
+			} ) ).to.deep.equal( {
+				666: { feed_ID: 666, is_error: true }
+			} );
+		} );
+
+		it( 'should overwrite an existing entry on receiving a new feed', () => {
+			const startingState = deepFreeze( {
+				666: { feed_ID: 666, blog_ID: 777, name: 'valid' }
+			} );
+			expect( items( startingState, {
 				type: READER_FEED_REQUEST_SUCCESS,
 				payload: {
-					feed_ID: 1,
-					blog_ID: 2
+					feed_ID: 666,
+					blog_ID: 888,
+					name: 'new',
+					subscribers_count: 10
 				}
-			} ).get( 1 )
-		).to.equal( {
-			feed_ID: 1,
-			blog_ID: 2
+			} ) ).to.deep.equal( {
+				666: {
+					feed_ID: 666,
+					blog_ID: 888,
+					name: 'new',
+					subscribers_count: 10
+				}
+			} );
+		} );
+
+		it( 'should leave an existing entry alone if an error is received', () => {
+			const startingState = deepFreeze( {
+				666: { feed_ID: 666, blog_ID: 777, name: 'valid' }
+			} );
+			expect( items( startingState, {
+				type: READER_FEED_REQUEST_FAILURE,
+				error: true,
+				payload: new Error( 'request failed' ),
+				meta: {
+					feed_ID: 666
+				}
+			} ) ).to.deep.equal( startingState );
 		} );
 	} );
 
-	it( 'should serialize feed entries', () => {
+	describe( 'isRequestingFeed', () => {
+		it( 'should add to the set of feeds inflight', () => {
 
-	} );
+		} );
 
-	it( 'should stash an error object in the map if the request fails', () => {
-	} );
+		it( 'should remove the feed from the set inflight', () => {
 
-	it( 'should overwrite an existing entry on receiving a new feed', () => {
-
-	} );
-
-	it( 'should leave an existing entry alone if an error is received', () => {
-
-	} );
-
-	it( 'should ', () => {
-
+		} );
 	} );
 } );
